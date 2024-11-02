6526cbe402e705da7093adfabb665b8f979356fa
debrix9 <dennis99.server@gmail.com>
2024-11-02 02:13:49 +0100

    stuff for first commit and random stuff to start

---
+++ utilsFuncs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace vincenty
+{
+    internal class utilsFuncs
+    {
+
+        public double flattening()
+        {
+            double f = (vincenty.constants.a - vincenty.constants.b) / vincenty.constants.a;
+            return f;
+        }
+    }
+}
---
+++ vincentyMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+
+
+
+namespace vincenty
+{
+
+    public static class constants
+    {
+        public const double a = 6378137.0;
+        public const double b = 6356752.314245;
+    }
+    class vincentyMain
+    {
+        
+
+        static void Main(string[] args)
+        {
+       
+            // Calculate flattening coefficient
+            // Example of processing arguments
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Arguments passed to the program:");
+                foreach (var arg in args)
+                {
+                    Console.WriteLine(arg);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No arguments were passed.");
+            }
+
+            // Keeping the console open
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static void DisplayCurrentDate()
+        {
+            Console.WriteLine($"Today's date is: {DateTime.Now:MMMM dd, yyyy}");
+        }
+    }
+}
